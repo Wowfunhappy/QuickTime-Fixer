c1d5790894331cf2f8acaf107a4501110559e9da
Wowfunhappy <wowfunhappy@gmail.com>
2020-12-28 22:02:26 -0500

    Attempts to fix volume but done for now

--- QuickTimeFixer/NSObject+Swizzling.m
+++ QuickTimeFixer/NSObject+Swizzling.m
@@ -6,8 +6,10 @@
 //  Copyright (c) 2020 Jonathan Alland. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import "NSObject+Swizzling.h"
 #import <Cocoa/Cocoa.h>
+#import <AVFoundation/AVFoundation.h>
 #import "ZKSwizzle.h"
 
 @interface myMGCinematicFrameView : NSView
@@ -15,6 +17,17 @@
     unsigned int doNotUse; //Without this, the next iVar seems to get messed up.
     bool needSetBackBufferDirty;
 }
+- (id)_subtreeDescription;
+@end
+
+@interface myQTHUDSliderCell : NSSlider
+- (void)accessibilityPerformAction:(id)arg1;
+- (id)accessibilityActionDescription:(id)arg1;
+- (id)accessibilityActionNames;
+- (void)accessibilitySetValue:(id)arg1 forAttribute:(id)arg2;
+- (BOOL)accessibilityIsAttributeSettable:(id)arg1;
+- (id)accessibilityAttributeValue:(id)arg1;
+- (id)accessibilityAttributeNames;
 @end
 
 @interface NSWindow (my)
@@ -59,6 +72,11 @@
     [[self window]update];
     
     ZKOrig(void);
+    
+    
+    //testing area
+    //NSLog(@"Subviews: %@", [self _subtreeDescription]);
+    
 }
 
 - (void)setTitle:(id)arg1 {
@@ -70,6 +88,58 @@
     return ([[self window] _canBecomeFullScreen] != NULL);
 }
 
+
+
+@end
+
+
+@implementation myQTHUDSliderCell
+
+- (void)stopTracking:(struct CGPoint)arg1 at:(struct CGPoint)arg2 inView:(id)arg3 mouseIsUp:(BOOL)arg4 {
+    if ([self isVolumeSlider]) {
+    }
+}
+
+- (double)_QTHUDSliderValidateUserValue:(double)arg1 {
+    /*NSString *str = [NSString stringWithFormat: @"set volume output volume %f * 100", [self floatValue]];
+    [[[NSAppleScript alloc] initWithSource:str] executeAndReturnError:nil];*/
+    
+    return ZKOrig(double, arg1);
+}
+
+- (BOOL)isVolumeSlider {
+    return ([self maxValue] == 1.000000);
+}
+
+- (double) getCurrSystemVolume {
+    NSAppleEventDescriptor *result = [[[NSAppleScript alloc] initWithSource:@"return (output volume of (get volume settings))"] executeAndReturnError:nil];
+    double volume = [[NSNumber numberWithInt:[result int32Value]] doubleValue] * 0.01;
+    return volume;
+}
+
+- (id)initWithCoder:(id)arg1 {
+    
+    NSLog(@"I'm here");
+    
+    AudioDeviceID device;
+    UInt32 size = sizeof(AudioDeviceID);
+    AudioHardwareGetProperty(kAudioHardwarePropertyDefaultOutputDevice, &size, &device);
+    UInt32 gChannels[2];
+    AudioDeviceGetProperty(device, 0, false, kAudioDevicePropertyPreferredChannelsForStereo, &size, gChannels);
+    
+    AudioDeviceAddPropertyListener(device, gChannels[0], false, kAudioDevicePropertyVolumeScalar, (AudioDevicePropertyListenerProc) volumeChangedCallback, (__bridge void *)(self));
+    
+    return ZKOrig (id, arg1);
+}
+
+OSStatus volumeChangedCallback ( AudioDeviceID inDevice, UInt32 inChannel, Boolean isInput, AudioDevicePropertyID inPropertyID, void* inClientData) {
+    objc_msgSend((__bridge id)(inClientData), @selector(volumeChanged), nil);
+    return 0;
+}
+- (void) volumeChanged {
+    [self setFloatValue:[self getCurrSystemVolume]];
+}
+
 @end
 
 
@@ -78,6 +148,8 @@
 
 + (void)load {
     ZKSwizzle(myMGCinematicFrameView, MGCinematicFrameView);
+    ZKSwizzle(myQTHUDSliderCell, QTHUDSliderCell);
+    
     
     //Fix menu bar not switching to QuickTime
     [[[NSAppleScript alloc] initWithSource:@"tell application (path to frontmost application as text) to activate"] executeAndReturnError:nil];
